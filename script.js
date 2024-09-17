4e6fefed392c7fda92cb7d7ad4a68a5ba366cec5
Dakhani Usman <dakhaniusman@gmail.com>
2024-09-17 13:19:32 +0530

    final updates

--- script.js
+++ script.js
@@ -1,32 +1,60 @@
-let input = document.getElementById('input-box');
-let buttons = document.querySelectorAll('button');
+// let input = document.getElementById('input-box');
+// let buttons = document.querySelectorAll('button');
+
+// let string = "";
+// let arr = Array.from(buttons);
+
+// // console.log(arr);
+
+// arr.forEach(button => {
+//     addEventListener('click', (e) => {
+//         if(e.target.innerHTML == '='){
+//             string = eval(string);
+//             input.value = string;
+//         }
+//         else if(e.target.innerHTML == 'AC'){
+//             string = "";
+//             input.value = string;
+//         }
+//         else if(e.target.innerHTML == 'DEL'){
+//             string = string.substring(0, string.length-1);
+//             input.value = string;
+//         }
+
+//         else{
+//             string += e.target.innerHTML;
+//             input.value = string;
+//         }
+//     })
+
+// })
 
 let string = "";
-let arr = Array.from(buttons);
+let buttons = document.querySelectorAll("button");
+// console.log(buttons);
 
-// console.log(arr);
-
-arr.forEach(button => {
-    addEventListener('click', (e) => {
-        if(e.target.innerHTML == '='){
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+        if (e.target.innerHTML == "="){
             string = eval(string);
-            input.value = string;
+            document.getElementById("input-box").value = string;
         }
-        else if(e.target.innerHTML == 'AC'){
+        else if(e.target.innerHTML == "AC"){
             string = "";
-            input.value = string;
+            document.getElementById("input-box").value = string;
         }
-        else if(e.target.innerHTML == 'DEL'){
-            string = string.substring(0, string.length-1);
-            input.value = string;
-        }
+        else if(e.target.innerHTML == "DEL"){
+            string = string.substring(0,string.length-1);
+            document.getElementById("input-box").value = string;
 
+        }
         else{
-            string += e.target.innerHTML;
-            input.value = string;
+            console.log(e.target);
+            string = string + e.target.innerHTML;
+            console.log(string)
+            document.getElementById("input-box").value = string;
         }
+
     })
-
 })
-
 
